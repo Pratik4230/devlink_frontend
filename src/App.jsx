b7e7e460c19c9400cd0539c4dd3885818a5a363d
Pratik4230 <pratikjadhav1438@gmail.com>
2024-11-11 13:04:28 +0530

    Start of Devlink_Frontend

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p className="bg-red-400">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Error,
+          labore? Blanditiis saepe nostrum ut, repellendus obcaecati officiis et
+          maxime fugit excepturi maiores!
+        </p>
+        <Button>HIIIIi</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
